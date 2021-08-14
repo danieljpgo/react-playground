9e89d063799dd2642ea2c69cb50a0cfd6e61416f
Daniel Jorge <djpgonzaga@gmail.com>
2021-08-13 23:49:44 -0300

    feat: api interceptor

---
+++ src/app/services/interceptor.ts
@@ -0,0 +1,28 @@
+import axios, { AxiosRequestConfig } from 'axios';
+import { getLocalStorageData, removeLocalStorageData } from '../common/utils';
+import type { Token } from '../common/types';
+
+export async function request(config: AxiosRequestConfig) {
+  const token = getLocalStorageData<Token>('token');
+
+  if (token && token['access-token']) {
+    return {
+      ...config,
+      headers: {
+        ...config.headers,
+        'access-token': token['access-token'],
+      },
+    };
+  }
+
+  return config;
+}
+
+export async function response(error: unknown) {
+  if (axios.isAxiosError(error) && error.response && [401, 403].includes(error.response.status)) {
+    removeLocalStorageData('headers');
+    window.location.reload();
+  }
+
+  Promise.reject(error);
+}
